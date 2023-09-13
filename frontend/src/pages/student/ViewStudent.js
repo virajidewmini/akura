96c70c82ff7545914916939e4a24ffee57eef133
Viraji Dewmini <virajidewmini@gmail.com>
2023-09-13 22:17:15 +0530

    Update ViewStudent.js

--- frontend/src/pages/student/ViewStudent.js
+++ frontend/src/pages/student/ViewStudent.js
@@ -6,7 +6,13 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-//import Navbar from '../../components/Navbar';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
@@ -22,6 +28,24 @@ const rows = [
 
 export default function BasicTable() {
   return (
+    <Box sx={{ display: 'flex' }}>
+    <CssBaseline />
+    <Navbar />
+    <Box
+        component="main"
+        sx={{
+        backgroundColor: (theme) =>
+            theme.palette.mode === 'light'
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+        }}
+    >
+        <Toolbar />
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
@@ -51,5 +75,10 @@ export default function BasicTable() {
         </TableBody>
       </Table>
     </TableContainer>
+    </Grid>
+        <Footer />
+        </Container>
+    </Box>
+    </Box>
   );
 }
