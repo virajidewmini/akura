a8da6cbf96e2928014c93497e2ea20263cdfc73a
bimsara1999 <blankanatha@gmail.com>
2023-09-14 23:35:04 +0545

    upt chk
    xx

--- frontend/src/pages/Checker/Checker.js
+++ frontend/src/pages/Checker/Checker.js
@@ -17,7 +17,7 @@ import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
 
 
-export default function Checker() {
+export default function Form() {
 
   const[firstName,setFirstName]=useState('')
   const[lastName,setLastName]=useState('')
